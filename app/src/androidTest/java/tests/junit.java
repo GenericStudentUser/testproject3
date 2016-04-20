82c0f76d3b113d2e8f27a4b725e2b002711a8972
Sean Yamane <syamane@ucsd.edu>
2016-04-20 06:44:37 -0700

    first

---
+++ app/src/androidTest/java/tests/junit.java
@@ -0,0 +1,27 @@
+package tests;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.TextView;
+
+import com.example.sean.lab3checkoffproject.MainActivity;
+import com.example.sean.lab3checkoffproject.R;
+
+/**
+ * Created by Sean on 4/20/2016.
+ */
+
+public class junit extends ActivityInstrumentationTestCase2<MainActivity>{
+
+    MainActivity mainActivity;
+    public junit(){
+        super (MainActivity.class);
+    }
+
+    public void firstTest(){
+        mainActivity = getActivity();
+        TextView textView = (TextView)mainActivity.findViewById(R.id.sum);
+        String tester = textView.getText().toString();
+
+        assertEquals("3", sum);
+    }
+}
